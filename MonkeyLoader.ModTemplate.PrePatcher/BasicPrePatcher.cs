6cbcdd9248c25d9a7dbb562bb0c5b4f51c139dbc
esnya <2088693+esnya@users.noreply.github.com>
2025-05-20 15:30:14 +0900

    Initial commit

---
+++ MonkeyLoader.ModTemplate.PrePatcher/BasicPrePatcher.cs
@@ -0,0 +1,38 @@
+﻿using MonkeyLoader.Patching;
+using Mono.Cecil.Cil;
+using Mono.Cecil.Rocks;
+using MonoMod.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyLoader.ModTemplate
+{
+    internal sealed class BasicPrePatcher : ConfiguredEarlyMonkey<BasicPrePatcher, MyConfig>
+    {
+        public static void HelloMethod()
+            => Logger.Info(() => $"Hello {ConfigSection.TargetName} from pre-patched-in SomeNameSpace.SomeType static constructor!");
+
+        // The options for these should be provided by your game's game pack.
+        protected override IEnumerable<IFeaturePatch> GetFeaturePatches() => Enumerable.Empty<IFeaturePatch>();
+
+        protected override IEnumerable<PrePatchTarget> GetPrePatchTargets()
+        {
+            yield return new PrePatchTarget(new AssemblyName("Assembly-CSharp"), "SomeNameSpace.SomeType");
+        }
+
+        protected override bool Patch(PatchJob patchJob)
+        {
+            var engine = patchJob.Types.First();
+            var engineCCtor = engine.GetStaticConstructor();
+
+            var processor = engineCCtor.Body.GetILProcessor(); // using MonoMod.Utils; is important for this to work v
+            processor.InsertBefore(engineCCtor.Body.Instructions.First(), processor.Create(OpCodes.Call, typeof(BasicPrePatcher).GetMethod(nameof(HelloMethod))));
+
+            patchJob.Changes = true;
+            return true;
+        }
+    }
+}
---
+++ MonkeyLoader.ModTemplate/BasicPatcher.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using MonkeyLoader.Patching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyLoader.ModTemplate
+{
+    [HarmonyPatch("SomeType", "SomeMethod")]
+    [HarmonyPatchCategory(nameof(BasicPatcher))]
+    internal sealed class BasicPatcher : Monkey<BasicPatcher>
+    {
+        // The options for these should be provided by your game's game pack.
+        protected override IEnumerable<IFeaturePatch> GetFeaturePatches() => Enumerable.Empty<IFeaturePatch>();
+
+        private static void Postfix()
+        {
+            Logger.Info(() => "Postfix for SomeType.SomeMethod()!");
+        }
+    }
+}
